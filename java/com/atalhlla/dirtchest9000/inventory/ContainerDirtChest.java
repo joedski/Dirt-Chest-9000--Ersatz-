b4a2f4fe2297ebb8efcdc75176d44acaeb77fec0
Joseph Sikorski <joedski@gmail.com>
2014-03-28 22:31:25 -0400

    Filled out the ContainerDirtChest class.

--- java/com/atalhlla/dirtchest9000/inventory/ContainerDirtChest.java
+++ java/com/atalhlla/dirtchest9000/inventory/ContainerDirtChest.java
@@ -1,14 +1,22 @@
 package com.atalhlla.dirtchest9000.inventory;
 
+import com.atalhlla.dirtchest9000.tileentity.TileEntityDirtChest;
+
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 
 public class ContainerDirtChest extends Container {
+	
+	private TileEntityDirtChest tileEntity;
+	
+	public ContainerDirtChest( InventoryPlayer playerInventory, TileEntityDirtChest tileEntity ) {
+		this.tileEntity = tileEntity;
+	}
 
 	@Override
-	public boolean canInteractWith( EntityPlayer var1 ) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean canInteractWith( EntityPlayer player ) {
+		return tileEntity.isUseableByPlayer( player );
 	}
 
 }
